d6a4fafaf1d17a66d1dd41ced6579c1ba100e38a
Ale557333 <alexandre.yukon@gmail.com>
2024-06-27 13:31:36 -0400

    files and layout

---
+++ src/app/layout.jsx
@@ -0,0 +1,16 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <header>
+          <h2>Análises de Jogos</h2>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>Rodapé</footer>
+      </body>
+    </html>
+  )
+}
+
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bem vindo(a) ao Análises de Jogos!</h1>
+    </main>
+  );
+}
