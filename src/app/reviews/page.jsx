a4133ab5c49e5b1e7b587d4adae4d6fa7bcef926
Ale557333 <alexandre.yukon@gmail.com>
2024-06-27 16:55:16 -0400

    add pages

---
+++ src/app/reviews/page.jsx
@@ -0,0 +1,10 @@
+const Reviews = () => {
+    return (
+        <div>
+          <h1>Análises</h1>
+            <p>Análises aqui</p>
+        </div>
+    )
+}
+
+export default Reviews
---
+++ src/app/reviews/super-mario-bros-wonder/page.jsx
@@ -0,0 +1,10 @@
+const SuperMarioBrosWonder = () => {
+    return (
+        <div>
+        <h1>Super Mario Bros. Wonder - Análise</h1>
+        <p>Análise de Super Mario Bros. Wonder aqui</p>
+        </div>
+    )
+    }
+
+    export default SuperMarioBrosWonder
