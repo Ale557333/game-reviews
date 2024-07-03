b0843f2efe5f7749741f013428100995b362147b
Ale557333 <alexandre.yukon@gmail.com>
2024-07-03 18:59:48 -0400

    adicionado imagens dos cards

--- src/app/analises/sonic-frontiers/page.jsx
+++ src/app/analises/sonic-frontiers/page.jsx
@@ -1,10 +1,19 @@
 import Heading1 from "@/app/components/heading-1"
+import Image from "next/image"
 
 const SonicFrontiers = () => {
     return (
         <>
-         <Heading1>Sonic Frontiers Análise</Heading1>
-         <p>Análise Sonic Frontiers aqui</p>
+          <Heading1>Sonic Frontiers Análise</Heading1>
+          <Image 
+            src="/images/sonic-frontiers.jpg" 
+            alt="Sonic Frontiers" 
+            width="640" 
+            height="360" 
+            priority 
+            className="rounded-lg mt-5 mb-5"
+          />
+          <p>Análise Sonic Frontiers aqui</p>
         </>
     )
 }
